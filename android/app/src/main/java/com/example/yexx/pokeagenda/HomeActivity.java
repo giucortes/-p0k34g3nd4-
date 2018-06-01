7d91726cafdb203dd21d21e49483fb66d9370b7c
Yeshua Braz <yeshua.braz@gmail.com>
2018-06-01 04:36:43 +0000

    Merge

--- android/app/src/main/java/com/example/yexx/pokeagenda/HomeActivity.java
+++ android/app/src/main/java/com/example/yexx/pokeagenda/HomeActivity.java
@@ -1,5 +1,4 @@
 package com.example.yexx.pokeagenda;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -17,7 +16,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
     }
 
-//    @Override
+    //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu){
 //        MenuInflater inflater = getMenuInflater();
 //        inflater.inflate(R.menu.menuzim, menu);
