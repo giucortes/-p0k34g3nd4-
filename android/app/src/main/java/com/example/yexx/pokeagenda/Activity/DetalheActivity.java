89d05ba1c0d286b8ec7835029d04e65ed401234e
Yeshua Braz <yeshua.braz@gmail.com>
2018-06-03 06:17:48 +0000

    front Detalhe

--- android/app/src/main/java/com/example/yexx/pokeagenda/Activity/DetalheActivity.java
+++ android/app/src/main/java/com/example/yexx/pokeagenda/Activity/DetalheActivity.java
@@ -1,23 +1,35 @@
 package com.example.yexx.pokeagenda.Activity;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.yexx.pokeagenda.R;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.drawable.RoundedBitmapDrawable;
-import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
 
 
 public class DetalheActivity extends AppCompatActivity {
+    private TextView txtEspeciePokemon;
+    private TextView txtNomePokemon;
+    private TextView txtNomeTreinador;
+    private TextView txtPesoPokemon;
+    private TextView txtAlturaPokemon;
+    private ImageView imagemDetalhe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhe);
+
+        // Init View
+        txtNomePokemon = (TextView) findViewById(R.id.txtNomePokemon);
+        txtEspeciePokemon = (TextView) findViewById(R.id.txtEspeciePokemon);
+        txtNomeTreinador = (TextView) findViewById(R.id.txtNomeTreinador);
+        txtPesoPokemon = (TextView) findViewById(R.id.txtPesoPokemon);
+        txtAlturaPokemon = (TextView) findViewById(R.id.txtAlturaPokemon);
+
+        imagemDetalhe = (ImageView) findViewById(R.id.imagemDetalhe);
 
 
     }
