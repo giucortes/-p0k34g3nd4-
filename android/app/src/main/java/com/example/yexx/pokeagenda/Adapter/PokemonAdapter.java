c3a8540bc3919994a586c29d0daee91a7b347a47
rodtwo <rodtwo@hotmail.com>
2018-06-03 03:15:17 -0300

    um dia funciona as img

--- android/app/src/main/java/com/example/yexx/pokeagenda/Adapter/PokemonAdapter.java
+++ android/app/src/main/java/com/example/yexx/pokeagenda/Adapter/PokemonAdapter.java
@@ -2,6 +2,7 @@ package com.example.yexx.pokeagenda.Adapter;
 
 import android.content.Context;
 import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,19 +12,33 @@ import android.widget.TextView;
 
 import com.example.yexx.pokeagenda.Model.Pokemon;
 import com.example.yexx.pokeagenda.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
 
 
 public class PokemonAdapter extends ArrayAdapter<Pokemon> {
 
     private ArrayList<Pokemon> pokemon;
     private Context context;
+    private ImageView imagemPokemonLista;
+
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
 
     public PokemonAdapter(Context c, ArrayList<Pokemon> objects) {
         super(c,0, objects);
         this.context = c;
         this.pokemon = objects;
+        // Init Firebase
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
     }
 
     @Override
@@ -35,7 +50,7 @@ public class PokemonAdapter extends ArrayAdapter<Pokemon> {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
             view = inflater.inflate(R.layout.list_cell, parent, false);
 
-            ImageView imagemPokemonLista = (ImageView) view.findViewById(R.id.imagemPokemonLista);
+            imagemPokemonLista = (ImageView) view.findViewById(R.id.imagemPokemonLista);
             TextView nomePokemonLista = (TextView) view.findViewById(R.id.nomePokemonLista);
             TextView  nomeTreinadorLista = (TextView) view.findViewById(R.id.nomeTreinadorLista);
 
@@ -44,7 +59,22 @@ public class PokemonAdapter extends ArrayAdapter<Pokemon> {
             if (pokemon2.getTreinadorCadastrou() != null) {
                 nomeTreinadorLista.setText(pokemon2.getTreinadorCadastrou().getEmail());    // TODO colocar nome
             }
-            imagemPokemonLista.setImageURI( new Uri.Builder().path(pokemon2.getFoto()).build() );
+
+            // pegar URL de download
+            Log.d("PIC", pokemon2.getFoto());
+            storageRef.child( pokemon2.getFoto() ).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    imagemPokemonLista.setImageURI( uri );
+                    Log.d("URI", uri.toString());
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    exception.printStackTrace();
+                }
+            });
+
         }
         return view;
     }
